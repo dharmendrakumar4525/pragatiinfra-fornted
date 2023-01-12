d17dea1f9be6291e3131550f5746f16b3a35a129
safiya <safiyasadeef@gmail.com>
2023-01-12 16:33:18 +0530

    projectscreenwork

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,12 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+ 
 
+  cards = [
+    {
+      title: ' Total  Projects',
+      count:'22'
+    
+    },
+    {
+      title: 'Completed Projects',
+      count:'10'
+    },
+    {
+      title: 'Incomplete Projects',
+      count:'5'
+    },
+    {
+      title: 'Overdue Projects',
+      count:'2'
+    },
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
