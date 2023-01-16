94340f5a9959d42f16c4360473eed41748b2676b
rami reddy nusum <ramireddy465@gmail.com>
2023-01-16 09:35:27 +0530

    add project check box

--- src/app/pages/add-project/add-project.component.ts
+++ src/app/pages/add-project/add-project.component.ts
@@ -6,9 +6,13 @@ import { MatDialog } from '@angular/material/dialog';
 import { AddTasksComponent } from '../add-tasks/add-tasks.component';
 import { TaskService } from 'src/app/services/task.service';
 import { AddSubTasksComponent } from '../add-sub-tasks/add-sub-tasks.component';
+import { AddProjectService } from 'src/app/services/add-project.service';
 
 export interface Fruit {
   name: string;
+}
+export interface MatchData {
+  id:string
 }
 @Component({
   selector: 'app-add-project',
@@ -21,37 +25,29 @@ export class AddProjectComponent implements OnInit {
   selectable = true;
   removable = true;
   addOnBlur = true;
+  selection: any[] = [];
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
   fruits: Fruit[] = [
     {name: 'Lemon'},
     {name: 'Lime'},
     {name: 'Apple'},
   ];
-
-  memberShipForm: FormGroup = this._fb.group({
-    membershipType: [null, [Validators.required]],
+  
+  projectForm: FormGroup = this._fb.group({
+    projectName: [null, [Validators.required]],
    // membership: [null],
-    membershipPlan: [null, [Validators.required]],
-    firstName: [null, [Validators.required, Validators.pattern('.*\\S.*[a-zA-Z ]')]],
-    lastName: [null, [Validators.required, Validators.pattern('.*\\S.*[a-zA-Z ]')]],
-    speciality: [null, [Validators.required, Validators.pattern('.*\\S.*[a-zA-Z ]')]],
-    password: [null, [Validators.required, Validators.minLength(6)]],
-    confPassword: [null, [Validators.required]],
+   projectDate: [null, [Validators.required]],
+   location: [null],
+   fruits: this._fb.array(["Lemon", "Lime", "Apple"]),
+   r0Date: [null],
+   r1Date: [null],
+   r2Date: [null],
   //  address: [null, [Validators.required]],
-    email: [null, [Validators.required, Validators.pattern(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)]],
-    phone: [null, [Validators.required, Validators.pattern(/^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$/)]],
-    profileUrl: [null],
-    studentDetails:[null],
-    addressLineOne:[null,[Validators.required]],
-    addressLineTwo:[null,[Validators.required]],
-    city:[null,[Validators.required]],
-    state:[null,[Validators.required]],
-    postalCode:[null,[Validators.required]]
-
 
   });
   tasks:any;
-  constructor(private _fb: FormBuilder, private _dialog: MatDialog,private taskService: TaskService,) { }
+  constructor(private _fb: FormBuilder, private _dialog: MatDialog,private taskService: TaskService,
+    private projectService: AddProjectService,) { }
 
   ngOnInit(): void {
   
@@ -63,6 +59,10 @@ export class AddProjectComponent implements OnInit {
     
   }
 
+  get courseIds() { 
+    return this.projectForm.get('courseIds');
+}
+
   add(event: MatChipInputEvent): void {
     const input = event.input;
     const value = event.value;
@@ -70,6 +70,9 @@ export class AddProjectComponent implements OnInit {
     // Add our fruit
     if ((value || '').trim()) {
       this.fruits.push({name: value.trim()});
+      // this.courseIds.value.push(value);
+
+      // this.courseIds.updateValueAndValidity();
     }
 
     // Reset the input value
@@ -116,5 +119,59 @@ export class AddProjectComponent implements OnInit {
       if (status === 'no') {
       }
     })
+  }
+  getSelection(item:any,task:any) {
+    return this.selection.findIndex(s => s._id === item._id) !== -1;
+  }
+
+  changeHandler(item: any, task:any) {
+    item.taskName = task.taskName
+    const id = item._id;
+    
+    console.log(task)
+
+    const index = this.selection.findIndex(u => u._id === id);
+    if (index === -1) {
+      // ADD TO SELECTION
+      // this.selection.push(item);
+      this.selection = [...this.selection, item];
+    } else {
+      // REMOVE FROM SELECTION
+      this.selection = this.selection.filter(user => user._id !== item._id)
+      // this.selection.splice(index, 1)
+    }
+  }
+
+  save() {
+    console.log(this.selection);
+  }
+
+  addProject(){
+    console.log(this.selection);
+    this.projectForm.value.sections = this.selection
+    this.projectService.addProject(this.projectForm.value).subscribe(
+
+      {
+        next: (data: any) =>  {
+          console.log(data)
+          // this.spinner.hide()
+          // this.router.navigate(['/usersList']);
+          // this.toast.openSnackBar('User Added Successfully');
+          
+        },
+        error: (err) => {
+          // this.spinner.hide()
+          // this.toast.openSnackBar('Something went wrong, please try again later');
+          // console.log(err) 
+  
+          // this.errorData = err
+  
+          
+  
+        }
+      }
+  
+    )
+
   }
 }
