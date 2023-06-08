a35460ff96a2efc16c8240b71fe57c0756f122e7
dharmendra kumar <dharmendra@avidusinteractive.com>
2023-06-08 13:40:56 +0530

    fix url  both file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  local_connection  :  'https://pr.avidusinteractive.com/api',
+  local_connection  :  'https://d2yt4av5mvk7ck.cloudfront.net/api',
   aws_connection  :  'https://pr.avidusinteractive.com/api'
 };
