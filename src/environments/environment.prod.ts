94fcd7bd4f8e87501a063834834a1fa414e77fc2
Shivangi Panchal <shivangipanchal1996@gmail.com>
2023-06-13 18:55:38 +0530

    calendar point done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  local_connection  :  'https://d2yt4av5mvk7ck.cloudfront.net/api',
+  local_connection  :  'https://pr.avidusinteractive.com/api',
   aws_connection  :  'https://pr.avidusinteractive.com/api'
 };
