a38def446cc157d6eadb653085ccda1d478b4aa0
Anshika <anshika.mishra1297@gmail.com>
2024-08-06 16:19:14 +0530

    Item Bulk Upload

--- src/app/components/item/listing/listing.component.ts
+++ src/app/components/item/listing/listing.component.ts
@@ -1,176 +1,5 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Router, ActivatedRoute } from '@angular/router';
-// import { ITEM_API } from '@env/api_path';
-// import { ExcelService } from '@services/export-excel/excel.service';
-// import { RequestService } from '@services/https/request.service';
-// import { SnackbarService } from '@services/snackbar/snackbar.service';
-// import { isEmpty } from 'lodash';
-// import { HttpClient } from '@angular/common/http';
-// import { environment } from '@env/environment';
-// import { CATEGORY_API,SUB_CATEGORY_API} from '@env/api_path';
-// @Component({
-//   selector: 'app-listing',
-//   templateUrl: './listing.component.html',
-//   styleUrls: ['./listing.component.scss']
-// })
-// export class ListingComponent implements OnInit {
-//   categoryList: any;
-//   subCategoryList: any = [];
-//   itemList: any = [];
-//   list: any;
-//   constructor(
-//     private router: Router,
-//     private httpService: RequestService,
-//     private excelService: ExcelService,
-//     private snack: SnackbarService,
-//     private route: ActivatedRoute,
-//     private http: HttpClient
-//   ) {
-//     this.getList();
-//   }
-
-//   getList() {
-//     const subCategory = this.http.get<any>(`${environment.api_path}${SUB_CATEGORY_API}`);
-//     const category = this.http.get<any>(`${environment.api_path}${CATEGORY_API}`);
-
-//     this.httpService.multipleRequests([subCategory, category], {}).subscribe(res => {
-//       if (res) {
-//         this.subCategoryList = res[0].data;
-//         console.log("Sub Category : ",this.subCategoryList);
-//         this.categoryList = res[1].data;
-//       }
-//     }, (err) => {
-//       if (err.errors && !isEmpty(err.errors)) {
-//         let errMessage = '<ul>';
-//         for (let e in err.errors) {
-//           let objData = err.errors[e];
-//           errMessage += `<li>${objData[0]}</li>`;
-//         }
-//         errMessage += '</ul>';
-//         this.snack.notifyHtml(errMessage, 2);
-//       } else {
-//         this.snack.notify(err.message, 2);
-//       }
-
-//     })
-  
-
-//     this.httpService.GET(ITEM_API, {}).subscribe(res => {
-//       console.log("res+++",res);
-//       if (res && res.data) {
-//         this.itemList = res.data;
-//         this.list = res.data;
-//       }
-//     }, (err) => {
-//       if (err.errors && !isEmpty(err.errors)) {
-//         let errMessage = '<ul>';
-//         for (let e in err.errors) {
-//           let objData = err.errors[e];
-//           errMessage += `<li>${objData[0]}</li>`;
-//         }
-//         errMessage += '</ul>';
-//         this.snack.notifyHtml(errMessage, 2);
-//       } else {
-//         this.snack.notify(err.message, 2);
-//       }
-
-//     })
-//   }
-
-//   getCategory(id: any) {
-//     return this.categoryList.filter((obj: { _id: any; }) => obj._id == id)[0]?.name
-//   }
-
-//   getSubCategory(id: any) {
-//     return this.subCategoryList.filter((obj: { _id: any; }) => obj._id == id)[0]?.subcategory_name
-//   }
-
-
-//   edit(id: any) {
-//     let url: string = "item/edit/" + id
-//     console.log(url);
-
-//     this.router.navigateByUrl(url);
-//   }
-
-//   add() {
-//     let url: string = "item/add"
-//     this.router.navigateByUrl(url);
-//   }
-
-//   delete(id: any) {
-//     this.httpService.DELETE(ITEM_API, { _id: id }).subscribe(res => {
-//       if (res) {
-//         this.snack.notify("item record has been deleted sucessfully.", 1);
-//         this.getList();
-//       }
-//     }, (err) => {
-//       if (err.errors && !isEmpty(err.errors)) {
-//         let errMessage = '<ul>';
-//         for (let e in err.errors) {
-//           let objData = err.errors[e];
-//           errMessage += `<li>${objData[0]}</li>`;
-//         }
-//         errMessage += '</ul>';
-//         this.snack.notifyHtml(errMessage, 2);
-//       } else {
-//         this.snack.notify(err.message, 2);
-//       }
-
-//     })
-//   }
-
-//   search(event: any) {
-//     if (event.target.value) {
-//       this.itemList = this.list.filter(obj => obj.item_name.toLowerCase().includes(event.target.value.toLowerCase()))
-//     }
-//     else {
-//       this.itemList = this.list;
-//     }
-//   }
-
-//   async exportXlSX() {
-
-//     let filterReport = this.itemList.map((o: any) => {
-//       o.categoryName = o.categoryDetail?.name
-//       o.subCategoryName = o.subCategoryDetail?.subcategory_name
-//       o.uomName = o.uomDetail?.uom_name
-//       o.gstValue = o.gstDetail?.gst_percentage
-
-//       return o;
-//     })
-//     let sheetHeaders = [
-//       "Item Number",
-//       "Item Name",
-//       "Category Name",
-//       "Sub Category Name",
-//       "UOM",
-//       "GST",
-//       "Specification"
-//     ];
-
-
-//     let valueKey = ['item_number',
-//       'item_name',
-//       'categoryName',
-//       'subCategoryName',
-//       'uomName',
-//       'gstValue',
-//       'specification'];
-//     let valueDataType = ['string', 'string', 'string', 'string', 'string', 'string', 'string'];
-//     let sheetName: any = "Item";
-//     this.excelService.mapArrayToExcel(sheetName, sheetHeaders, valueKey, valueDataType, filterReport);
-//   }
-
-//   ngOnInit(): void {
-//   }
-
-// }
-
-
-
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ViewChild,TemplateRef  } from '@angular/core';
+import { Router } from '@angular/router';
 import { ITEM_API } from '@env/api_path';
 import { ExcelService } from '@services/export-excel/excel.service';
 import { RequestService } from '@services/https/request.service';
@@ -181,6 +10,8 @@ import { environment } from '@env/environment';
 import { CATEGORY_API, SUB_CATEGORY_API } from '@env/api_path';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { ToastService } from '@services/toast.service';
+import { MatDialog } from '@angular/material/dialog'; // Import MatDialog
+
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
@@ -192,47 +23,52 @@ export class ListingComponent implements OnInit {
   editPermission: any;
   addPermission: any;
   deletePermission: any;
-  permissions:any;
+  permissions: any;
   categoryList: any;
   subCategoryList: any = [];
   itemList: any = [];
   list: any;
   pageSizeOptions: number[] = [5, 10, 20];
   pageSize: number = 10;
-  length=50;
+  length = 50;
   pageIndex = 0;
   disabled = false;
   showPageSizeOptions = false;
   showFirstLastButtons = true;
   hidePageSize = false;
-  pagestore=0
+  pagestore = 0;
+
+
+  @ViewChild('errorDialog') errorDialog: TemplateRef<any>;
+
   constructor(
     private router: Router,
     private httpService: RequestService,
     private excelService: ExcelService,
     private snack: SnackbarService,
-    private route: ActivatedRoute,
     private http: HttpClient,
-    private toast:ToastService
+    private toast: ToastService,
+    public dialog: MatDialog
   ) { }
+
   ngOnInit(): void {
-    this.permissions = JSON.parse(localStorage.getItem('loginData'))
-    const rolePermission = this.permissions.user.role
+    this.permissions = JSON.parse(localStorage.getItem('loginData'));
+    const rolePermission = this.permissions.user.role;
     const GET_ROLE_API_PERMISSION = `/roles/role/${rolePermission}`;  
-      this.httpService.GET(GET_ROLE_API_PERMISSION,{}).subscribe({
-        next: (resp: any) => {
-          this.addPermission=resp.dashboard_permissions[0].ParentChildchecklist[11].childList[0].isSelected;
-          this.editPermission=resp.dashboard_permissions[0].ParentChildchecklist[11].childList[1].isSelected;
-          this.deletePermission=resp.dashboard_permissions[0].ParentChildchecklist[11].childList[2].isSelected;
-          this.viewPermission=resp.dashboard_permissions[0].ParentChildchecklist[11].childList[3].isSelected;
-        
-        },
-        error: (err) => {
-          console.log(err)
-        }
-      });
+    this.httpService.GET(GET_ROLE_API_PERMISSION, {}).subscribe({
+      next: (resp: any) => {
+        this.addPermission = resp.dashboard_permissions[0].ParentChildchecklist[11].childList[0].isSelected;
+        this.editPermission = resp.dashboard_permissions[0].ParentChildchecklist[11].childList[1].isSelected;
+        this.deletePermission = resp.dashboard_permissions[0].ParentChildchecklist[11].childList[2].isSelected;
+        this.viewPermission = resp.dashboard_permissions[0].ParentChildchecklist[11].childList[3].isSelected;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
     this.getList();
   }
+
   getList() {
     const subCategory = this.http.get<any>(`${environment.api_path}${SUB_CATEGORY_API}`);
     const category = this.http.get<any>(`${environment.api_path}${CATEGORY_API}`);
@@ -254,12 +90,12 @@ export class ListingComponent implements OnInit {
       } else {
         this.snack.notify(err.message, 2);
       }
-    })
+    });
     this.fetchItemList(0, this.pageSize);
     this.httpService.GET(ITEM_API, {}).subscribe(res => {
       if (res && res.data) {
         this.list = res.data;
-        this.length=res.data.length
+        this.length = res.data.length;
       }
     }, (err) => {
       if (err.errors && !isEmpty(err.errors)) {
@@ -275,10 +111,11 @@ export class ListingComponent implements OnInit {
       }
     });
   }
+
   fetchItemList(page: number, pageSize: number) {
     const params = { page: page + 1, per_page: pageSize };
-    this.pagestore=params.page
-    console.log("L",this.pagestore)
+    this.pagestore = params.page;
+    console.log("L", this.pagestore);
     this.httpService.GET(ITEM_API, params).subscribe(res => {
       console.log("res+++", res);
       if (res && res.data) {
@@ -298,40 +135,42 @@ export class ListingComponent implements OnInit {
       }
     });
   }
+
   getCategory(id: any) {
-    return this.categoryList.filter((obj: { _id: any; }) => obj._id == id)[0]?.name
+    return this.categoryList.filter((obj: { _id: any; }) => obj._id == id)[0]?.name;
   }
+
   getSubCategory(id: any) {
-    return this.subCategoryList.filter((obj: { _id: any; }) => obj._id == id)[0]?.subcategory_name
+    return this.subCategoryList.filter((obj: { _id: any; }) => obj._id == id)[0]?.subcategory_name;
   }
+
   edit(id: any) {
-    if(!this.editPermission)
-    {
+    if (!this.editPermission) {
       this.toast.openSnackBar('Access to Item Master editing is restricted for your account.');
       return;
     }
-    let url: string = "item/edit/" + id
+    let url: string = "item/edit/" + id;
     console.log(url);
     this.router.navigateByUrl(url);
   }
+
   add() {
-    if(!this.addPermission)
-    {
+    if (!this.addPermission) {
       this.toast.openSnackBar('Access to Item Master add is restricted for your account.');
       return;
     }
-    let url: string = "item/add"
+    let url: string = "item/add";
     this.router.navigateByUrl(url);
   }
+
   delete(id: any) {
-    if(!this.deletePermission)
-    {
+    if (!this.deletePermission) {
       this.toast.openSnackBar('Access to Item Master deleting is restricted for your account.');
       return;
     }
     this.httpService.DELETE(ITEM_API, { _id: id }).subscribe(res => {
       if (res) {
-        this.snack.notify("item record has been deleted sucessfully.", 1);
+        this.snack.notify("Item record has been deleted successfully.", 1);
         this.getList();
       }
     }, (err) => {
@@ -346,24 +185,25 @@ export class ListingComponent implements OnInit {
       } else {
         this.snack.notify(err.message, 2);
       }
-    })
+    });
   }
+
   search(event: any) {
     if (event.target.value) {
-      this.itemList = this.list.filter(obj => obj.item_name.toLowerCase().includes(event.target.value.toLowerCase()))
-    }
-    else {
-      this.fetchItemList(this.pagestore-1, this.pageSize);
+      this.itemList = this.list.filter(obj => obj.item_name.toLowerCase().includes(event.target.value.toLowerCase()));
+    } else {
+      this.fetchItemList(this.pagestore - 1, this.pageSize);
     }
   }
+
   async exportXlSX() {
     let filterReport = this.itemList.map((o: any) => {
-      o.categoryName = o.categoryDetail?.name
-      o.subCategoryName = o.subCategoryDetail?.subcategory_name
-      o.uomName = o.uomDetail?.uom_name
-      o.gstValue = o.gstDetail?.gst_percentage
+      o.categoryName = o.categoryDetail?.name;
+      o.subCategoryName = o.subCategoryDetail?.subcategory_name;
+      o.uomName = o.uomDetail?.uom_name;
+      o.gstValue = o.gstDetail?.gst_percentage;
       return o;
-    })
+    });
     let sheetHeaders = [
       "Item Number",
       "Item Name",
@@ -384,8 +224,46 @@ export class ListingComponent implements OnInit {
     let sheetName: any = "Item";
     this.excelService.mapArrayToExcel(sheetName, sheetHeaders, valueKey, valueDataType, filterReport);
   }
+
   onPageChange(event: PageEvent) {
     console.log(event);
     this.fetchItemList(event.pageIndex, event.pageSize);
   }
+
+  importFile(event: any) {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    this.http.post(`${environment.api_path}/item/upload-csv`, formData).subscribe({
+      next: (res: any) => {
+        this.snack.notify('File imported successfully!', 2);
+        this.getList();
+      },
+      error: (err) => {
+        console.log("Error:", err.error.message);
+        const message = err.error.message;
+
+        // Remove the leading part of the message and split by commas
+        const errorParts = message
+          .replace('Errors encountered during processing:', '') // Remove the leading message
+          .split(',')  // Split by commas
+          .map(error => `<li>${error.trim()}</li>`)  // Trim and format each error
+          .join('');
+
+        const errorHtml = `<ul>${errorParts}</ul>`;
+        this.showErrorDialog(errorHtml);
+      }
+    });
+  }
+
+  showErrorDialog(message: string) {
+    console.log(message);
+    this.dialog.open(this.errorDialog, {
+      data: { message: message }
+    });
+  }
+
 }
